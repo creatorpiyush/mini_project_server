95ce47d62ed04e1e9fea01431f15901264aa21e2
creatorpiyush <piyushanand.1221@gmail.com>
2021-04-04 23:34:24 +0530

    online mongodb added via .env and configvars

--- model/index.js
+++ model/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const db_url = "mongodb://localhost:27017/mini_project_db";
+require("dotenv").config();
+
+const db_url = process.env.DB_URL;
 
 mongoose.connect(
     db_url, {
